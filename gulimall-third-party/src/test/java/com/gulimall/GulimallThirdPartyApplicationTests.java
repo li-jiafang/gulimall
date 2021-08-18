579f8360a758d16a251db12a15720ed1cb994294
ljf <ljfsunlight@gmail.com>
2021-08-18 17:56:32 +0800

    第三方服务配置

--- gulimall-third-party/src/test/java/com/gulimall/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/gulimall/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,42 @@
 package com.gulimall;
 
-import org.junit.jupiter.api.Test;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
 
+
+    @Resource
+    private OSSClient ossClient;
+
+
     @Test
-    void contextLoads() {
+    public void testUpload() throws FileNotFoundException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        String accessKeyId = "LTAI5tLfD3GZo2qKNZ9a7Lgb";
+        String accessKeySecret = "fPmXL0uL0qpqvKKP6CnzcnAuKo4ITO";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        //上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\26039\\Pictures\\wallhaven-3kpvkd.jpg");
+        ossClient.putObject("gulimall-ljf123", "333.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功.");
     }
 
 }
