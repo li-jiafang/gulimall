24681db5130bba925a6b0bc1a77fd1b7f58f9c42
ljf <ljfsunlight@gmail.com>
2021-09-02 16:29:21 +0800

    99 仓库服务 完成采购

--- gulimall-ware/src/main/java/com/gulimall/ware/service/PurchaseService.java
+++ gulimall-ware/src/main/java/com/gulimall/ware/service/PurchaseService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.gulimall.common.utils.PageUtils;
 import com.gulimall.ware.entity.PurchaseEntity;
 import com.gulimall.ware.vo.MergeVo;
+import com.gulimall.ware.vo.PurchaseDoneVo;
 
 import java.util.List;
 import java.util.Map;
@@ -24,5 +25,7 @@ public interface PurchaseService extends IService<PurchaseEntity> {
     void mergePurchase(MergeVo mergeVo);
 
     void received(List<Long> ids);
+
+    void done(PurchaseDoneVo doneVo);
 }
 
--- gulimall-ware/src/main/java/com/gulimall/ware/service/impl/PurchaseServiceImpl.java
+++ gulimall-ware/src/main/java/com/gulimall/ware/service/impl/PurchaseServiceImpl.java
@@ -3,10 +3,14 @@ package com.gulimall.ware.service.impl;
 import com.gulimall.common.constant.WareConstant;
 import com.gulimall.ware.entity.PurchaseDetailEntity;
 import com.gulimall.ware.service.PurchaseDetailService;
+import com.gulimall.ware.service.WareSkuService;
 import com.gulimall.ware.vo.MergeVo;
+import com.gulimall.ware.vo.PurchaseDoneVo;
+import com.gulimall.ware.vo.PurchaseItemDoneVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +33,9 @@ public class PurchaseServiceImpl extends ServiceImpl<PurchaseDao, PurchaseEntity
 
     @Autowired
     PurchaseDetailService detailService;
+
+    @Autowired
+    WareSkuService wareSkuService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -126,6 +133,45 @@ public class PurchaseServiceImpl extends ServiceImpl<PurchaseDao, PurchaseEntity
             detailService.updateBatchById(detailEntities);
         });
     }
+
+    @Transactional
+    @Override
+    public void done(PurchaseDoneVo doneVo) {
+
+        Long id = doneVo.getId();
+
+
+        //2、改变采购项的状态
+        Boolean flag = true;
+        List<PurchaseItemDoneVo> items = doneVo.getItems();
+
+        List<PurchaseDetailEntity> updates = new ArrayList<>();
+        for (PurchaseItemDoneVo item : items) {
+            PurchaseDetailEntity detailEntity = new PurchaseDetailEntity();
+            if(item.getStatus() == WareConstant.PurchaseDetailStatusEnum.HASERROR.getCode()){
+                flag = false;
+                detailEntity.setStatus(item.getStatus());
+            }else{
+                detailEntity.setStatus(WareConstant.PurchaseDetailStatusEnum.FINISH.getCode());
+                ////3、将成功采购的进行入库
+                PurchaseDetailEntity entity = detailService.getById(item.getItemId());
+                wareSkuService.addStock(entity.getSkuId(),entity.getWareId(),entity.getSkuNum());
+
+            }
+            detailEntity.setId(item.getItemId());
+            updates.add(detailEntity);
+        }
+
+        detailService.updateBatchById(updates);
+
+        //1、改变采购单状态
+        PurchaseEntity purchaseEntity = new PurchaseEntity();
+        purchaseEntity.setId(id);
+        purchaseEntity.setStatus(flag?WareConstant.PurchaseStatusEnum.FINISH.getCode():WareConstant.PurchaseStatusEnum.HASERROR.getCode());
+        purchaseEntity.setUpdateTime(new Date());
+        this.updateById(purchaseEntity);
+    }
+
 
 
 }
