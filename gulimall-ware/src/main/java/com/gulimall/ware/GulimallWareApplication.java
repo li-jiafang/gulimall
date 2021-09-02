78bb6f3ee0d63c89e02aaf4080b1758b0b7440bd
ljf <ljfsunlight@gmail.com>
2021-09-02 16:30:44 +0800

    增加feign服务注解

--- gulimall-ware/src/main/java/com/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/gulimall/ware/GulimallWareApplication.java
@@ -2,7 +2,11 @@ package com.gulimall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.gulimall.ware.feign")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
 
