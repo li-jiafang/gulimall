09dd62f947bb5c3ed159fc84ca000fafbda8bf21
ljf <ljfsunlight@gmail.com>
2021-09-06 14:30:12 +0800

    去除不必要的配置

--- gulimall-third-party/src/test/java/com/gulimall/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/gulimall/GulimallThirdPartyApplicationTests.java
@@ -19,24 +19,6 @@ class GulimallThirdPartyApplicationTests {
     private OSSClient ossClient;
 
 
-    @Test
-    public void testUpload() throws FileNotFoundException {
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tLfD3GZo2qKNZ9a7Lgb";
-        String accessKeySecret = "fPmXL0uL0qpqvKKP6CnzcnAuKo4ITO";
 
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-        //上传文件流。
-        InputStream inputStream = new FileInputStream("C:\\Users\\26039\\Pictures\\wallhaven-3kpvkd.jpg");
-        ossClient.putObject("gulimall-ljf123", "333.jpg", inputStream);
-
-        // 关闭OSSClient。
-        ossClient.shutdown();
-        System.out.println("上传成功.");
-    }
 
 }
